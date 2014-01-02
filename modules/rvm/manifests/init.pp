9701ee91a42514d7bf2267046f28fc76080cd53e
kwongtgam <kwong@globeandmail.com>
2014-01-02 14:15:07 -0500

    Add file to source rvm environment

--- modules/rvm/manifests/init.pp
+++ modules/rvm/manifests/init.pp
@@ -4,6 +4,11 @@ class rvm {
     ensure => installed,
   }
 
+  file { "/etc/profile.d/rvmenv":
+    ensure  => file,
+    content => template("rvm/rvmenv.erb"),
+  }
+
   exec { 'install_rvm':
     path    => ["/usr/bin", "/usr/sbin", "/usr/local/bin", "/bin", "/sbin"],
     command => "curl -L https://get.rvm.io | bash -s stable --ruby; touch /opt/rvminstalled",
